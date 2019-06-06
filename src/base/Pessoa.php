312d8165ca9d74a6d4a4051cfc27148fad77056b
Fabio Chicout <fchicout@gmail.com>
2019-06-06 09:52:45 -0300

    POST / and /id completed

--- src/base/Pessoa.php
+++ src/base/Pessoa.php
@@ -20,28 +20,44 @@ return function ($app, $container) {
             ->withStatus(200);
     });
 
-    // POST a new user to DB;
-    // REMEMBER: 2 Steps
-    $app->post('/pessoas/[id]', function (Request $request, Response $response, array $args) use ($container) {
+    // POST get next id
+    $app->post('/pessoas/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Pessoas App: [POST] pessoas/. Received Data: " . json_encode($args));
 
         $body = $request->getParsedBody();
         $connection = $this->db;
         $data = null;
-        if (isset($args['id'])) {
-
-            $stmt = $connection->prepare('INSERT INTO Users (user_login, user_password, user_salt) VALUES (?, ?, ?);');
-            $stmt->execute($body["user_login"], $body["user_password"], $body["user_salt"]);
-        } else {
-            $stmt = $connection->query('SELECT AUTO_INCREMENT id
-                                            FROM information_schema.TABLES 
-                                           WHERE TABLE_SCHEMA=\'AulaPW\' AND 
-                                                 TABLE_NAME=\'Users\';');
-            $data = $stmt->fetchAll();
-        }
+        $stmt = $connection->query('SELECT AUTO_INCREMENT id
+                                      FROM information_schema.TABLES 
+                                     WHERE TABLE_SCHEMA=\'AulaPW\' AND 
+                                           TABLE_NAME=\'Users\';');
+        $data = $stmt->fetchAll();
 
         return $response->withJson($data)
-            ->withStatus(200);
+                        ->withStatus(200);
+    });
+
+    // POST a new user to DB;
+    // REMEMBER: 2 Steps!!
+    $app->post('/pessoas/{id}', function (Request $request, Response $response, array $args) use ($container) {
+        
+        $body = $request->getParsedBody();
+        
+        // Sample log message
+        $container->get('logger')->info("Pessoas App: [POST] pessoas/. Received Data: " . json_encode($body));
+        $connection = $this->db;
+        $data = null;
+
+        $stmt = $connection->prepare('INSERT INTO Users (user_login, user_password, user_salt) VALUES (:ulogin, :upasswd, :usalt);');
+        $stmt->execute(array( 
+                ':ulogin'  => $body["user_login"], 
+                ':upasswd' => $body["user_password"],
+                ':usalt'   => $body["user_salt"])
+            );
+        $data = $stmt->rowCount();
+
+        return $response->withJson($data)
+                        ->withStatus(200);
     });
 };
